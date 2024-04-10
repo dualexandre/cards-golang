42f6fce63e02557fa839bc565ce51557daf43d5a
Eduardo Alexandre <luis.edu.alex@gmail.com>
2024-04-10 16:51:53 -0300

    feat: add main func

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Other ways of declaring variables
+	// var card string = "Ace of Spades"
+	card := "Ace of Spades"
+	card = "Five of Diamonds" // You can reassign the value of the variable, as long as the value is of the same type.
+	fmt.Println(card)
+}
+
+// Basic Go Types: bool, string, int, float64
